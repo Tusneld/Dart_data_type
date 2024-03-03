9d177a6cba8dd05852e8f6b326f359093a8ff38d
Tusnelde Endjala <tusneldelainae@gmail.com>
2024-03-03 20:43:54 +0200

    Dart data type

---
+++ datatype.dart
@@ -0,0 +1,30 @@
+//Dart program demonstrating the use of data types in Dart
+void main(){
+  //int data type
+  int num = 10;
+  print("The integer value is: $num");
+
+  //double data type
+  double height = 10.50;
+  print('Height: $height');
+
+  //String data type
+  String name = "Tusnelde";
+  print("The string value is: $name");
+
+  //List data type
+  List<String> names = ["Tusnelde", "Laina", "Shagwana", "Endjala"];
+  print("The list value is: $names");
+
+  //Map data type
+  Map<String, String> details = {
+    "name": "Tusnelde",
+    "age": "20",
+    "location": "Ongwediva"
+  };
+  print("The map value is: $details");
+
+}
+ 
+
+  
